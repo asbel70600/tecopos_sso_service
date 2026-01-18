988ba779054e67246d9c4f8c52509c60d6138831
asbel <asbel70600@gmail.com>
2026-01-18 05:56:48 -0500

    feat: added grpc server listenning

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { Transport } from "@nestjs/microservices";
+import { join } from "node:path";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(process.env["PORT"] ?? 3000);
+    const app = await NestFactory.createMicroservice(AppModule, {
+        transport: Transport.GRPC,
+        options: {
+            package: "sso",
+            protoPath: join(__dirname, "../proto/sso.proto"),
+            url: process.env["GRPC_URL"] || "0.0.0.0:50051",
+        },
+    });
+
+    await app.listen();
+    console.log(
+        "SSO gRPC server is listening on",
+        process.env["GRPC_URL"] || "0.0.0.0:50051",
+    );
 }
 bootstrap();
